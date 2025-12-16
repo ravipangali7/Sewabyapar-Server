7234a2691f4a10b6b4f5003cc5997d3ff5cac08c
Infelo <infelo453@gmail.com>
2025-12-17 00:13:56 +0545

    Changes

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -16,7 +16,7 @@ class Store(models.Model):
     latitude = models.DecimalField(max_digits=20, decimal_places=12, null=True, blank=True)
     longitude = models.DecimalField(max_digits=20, decimal_places=12, null=True, blank=True)
     phone = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
