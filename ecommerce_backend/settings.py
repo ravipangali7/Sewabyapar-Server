1221dd81474a388a00d449a652317dbfd492f9d2
Infelo <infelo453@gmail.com>
2025-12-31 16:22:48 +0545

    Changes

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -125,22 +125,22 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sewabyapar',
-        'USER': 'sewabyapar',
-        'PASSWORD': 'Sewabyapar453',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'sewabyapar',
+    #     'USER': 'sewabyapar',
+    #     'PASSWORD': 'Sewabyapar453',
+    #     'HOST': 'localhost',
+    #     'PORT': 3306,
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    #         'charset': 'utf8mb4',
+    #     },
+    # }
 }
 
 
