5c1d0b6c0c9f7185ba27c995ee8a5f3eca7964df
Infelo <infelo453@gmail.com>
2026-01-15 23:07:20 +0545

    Changes

--- website/views/ecommerce/shipment_views.py
+++ website/views/ecommerce/shipment_views.py
@@ -193,7 +193,7 @@ def _add_logo_to_manifest_pdf(pdf_url, logo_path):
             # Position logo at top-left (with some margin, above Courier area)
             # Increased gap from top to position logo lower (more downside)
             x_position = 45
-            y_position = page_height - logo_height - 50
+            y_position = page_height - logo_height - 65
             
             # Draw logo (fully opaque, 100% opacity)
             can.drawImage(
